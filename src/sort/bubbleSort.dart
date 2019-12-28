08386c1458b7ddf940a0f29859571054b32732cd
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2019-12-28 18:48:32 -0300

    implement bubble sort.

--- src/sort/bubbleSort.dart
+++ src/sort/bubbleSort.dart
@@ -1,3 +1,25 @@
-List<int> sort() {
-  return [ 1, 2, 3 ];
+import 'package:meta/meta.dart';
+
+List<int> sort({ @required List<int> numbers }) {
+  var swap = (list, idx1, idx2) {
+    // xor swap
+    list[idx1] ^= list[idx2];
+    list[idx2] ^= list[idx1];
+    list[idx1] ^= list[idx2];
+  };
+  var doSort = (numbers) {
+    var swapped;
+    do {
+      swapped = false;
+      for (int j = 0; j < numbers.length - 1; j++) {
+        if (numbers[j] > numbers[j + 1]) {
+          swap(numbers, j, j + 1);
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    return numbers;
+  };
+
+  return doSort(numbers);
 }
