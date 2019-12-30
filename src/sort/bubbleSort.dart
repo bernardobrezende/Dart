e3e3805160878761c2c38092408a1e3e8e75222d
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2019-12-29 22:43:51 -0300

    make closures final.

--- src/sort/bubbleSort.dart
+++ src/sort/bubbleSort.dart
@@ -1,13 +1,13 @@
 import 'package:meta/meta.dart';
 
 List<int> sort({ @required List<int> numbers }) {
-  var swap = (list, idx1, idx2) {
+  final swap = (list, idx1, idx2) {
     // xor swap
     list[idx1] ^= list[idx2];
     list[idx2] ^= list[idx1];
     list[idx1] ^= list[idx2];
   };
-  var doSort = (numbers) {
+  final doSort = (numbers) {
     var swapped;
     do {
       swapped = false;
